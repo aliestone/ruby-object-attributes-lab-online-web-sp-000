902059e84c7348e04614fb47cc18dfb567aba309
Alie Stone <helloaliestone@gmail.com>
2020-09-30 16:56:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 class Dog
   attr_accessor :name :breed
 
-def dog_with_names(name)
-  @name =name
-end
+  def dog_with_names(name)
+    @name = name
+  end
 
-def dog_with_breeds(breed)
-  @breed =breed
-end
-
+  def dog_with_breeds(breed)
+    @breed = breed
+  end
 
 end
